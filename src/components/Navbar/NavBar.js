932d312b86455688d3bb0663508ab6edf1f1c64c
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-07-04 18:59:43 -0300

    header marked up for Bscreen

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./NavBar.css";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <header>
+      <div className="header-container">
+        <div class="logo">
+          <h2>hello</h2>
+        </div>
+        <nav class="nav">
+          <ul>
+            <li>
+              <a href="#">DashBoard</a>
+            </li>
+            <li>
+              <a href="#">Clases</a>
+            </li>
+            <li>
+              <a href="#">Alumnos</a>
+            </li>
+            <li>
+              <a href="#">Profesores</a>
+            </li>
+            <li>
+              <a href="#">Pagos</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
