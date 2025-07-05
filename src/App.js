becb877624db313593fa156d34f655ff46d45775
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-07-04 22:58:33 -0300

    Routes graped

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import NavBar from "./Components/Navbar/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DashBoard from "./Pages/Dashboard";
+import Clases from "./Pages/Clases";
+import Alumnos from "./Pages/Alumnos";
+import Profesores from "./Pages/Profesores";
+import Pagos from "./Pages/Pagos";
 
 function App() {
   return (
-    <div>
+    <Router>
       <NavBar />
-    </div>
+      <Routes>
+        <Route path="/" element={<DashBoard />} />
+        <Route path="/clases" element={<Clases />} />
+        <Route path="/alumnos" element={<Alumnos />} />
+        <Route path="/profesores" element={<Profesores />} />
+        <Route path="/pagos" element={<Pagos />} />
+      </Routes>
+    </Router>
   );
 }
 
