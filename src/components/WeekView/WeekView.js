8e2f0c3ba84c5f15170240107f7facf6f828c996
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-07-15 00:19:55 -0300

    WeekView component added + styling

---
+++ src/components/WeekView/WeekView.js
@@ -0,0 +1,40 @@
+// src/components/WeekView.js
+import React from "react";
+import "./WeekView.css";
+
+function WeekView({ week, onSelectDay }) {
+  return (
+    <div className="week-grid">
+      {week.map((dayObj) => (
+        <div
+          key={dayObj.date}
+          className="day-col"
+          onClick={() => onSelectDay(dayObj.date)}
+        >
+          <div className="day-header">
+            {dayObj.date.getDate()}
+            <span>
+              {dayObj.date.toLocaleDateString("es-AR", { weekday: "short" })}
+            </span>
+          </div>
+
+          {dayObj.classes.length ? (
+            dayObj.classes.map((c) => (
+              <div key={c.id} className="mini-row">
+                <div className="mini-top">
+                  <span>{c.time}</span>
+                  <span>{c.circuit}</span>
+                </div>
+                <div className="mini-student">{c.student}</div>
+              </div>
+            ))
+          ) : (
+            <p className="no-class">Sin clases</p>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default WeekView;
