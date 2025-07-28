c3da55562ceccddde32d529abd02d60e1b88200c
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-07-28 02:22:27 -0300

    connection changed to production

--- Frontend/src/Pages/Login.js
+++ Frontend/src/Pages/Login.js
@@ -12,11 +12,14 @@ const Login = () => {
     e.preventDefault();
     setError("");
     try {
-      const response = await fetch("http://localhost:3001/api/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
-      });
+      const response = await fetch(
+        "https://academia-production-53e3.up.railway.app/api/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username, password }),
+        }
+      );
       const data = await response.json();
       if (response.ok) {
         localStorage.setItem("token", data.token);
