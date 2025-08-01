e1bc8aafd7a0b1574fb8d19dff771faa6aa85603
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-08-01 11:37:02 -0300

    Alumno model edited

--- Backend/src/models/alumno.ts
+++ Backend/src/models/alumno.ts
@@ -2,8 +2,15 @@ import mongoose from 'mongoose';
 
 const AlumnoSchema = new mongoose.Schema({
   nombre: String,
+  apellido: String,
+  telefono: String,
   email: String,
   edad: Number,
-});
+  clases_restantes: {type: Number, default: 0 },
+  fecha_creacion: {
+    type: String, // formato YYYY-MM-DD
+     default: () => new Date().toISOString().slice(0, 10),
+  },
+}, { timestamps: true });
 
 export default mongoose.model('Alumno', AlumnoSchema);
