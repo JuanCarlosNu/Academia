9f4c97d450112924dbaf5cba7052401708bae0ac
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-08-05 10:16:02 -0300

    alumno Controller corrected. Was writen for profesor

--- Backend/src/controllers/alumnoController.ts
+++ Backend/src/controllers/alumnoController.ts
@@ -1,22 +1,23 @@
+import alumno from "../models/alumno";
 import profesor from "../models/profesor";
 import { Request, Response } from "express";
 
-export const getProfesores = async (req: Request, res: Response) => {
+export const getAlumnos = async (req: Request, res: Response) => {
   try {
-    const profesores = await profesor.find();
-    res.status(200).json(profesores);
+    const alumnos = await alumno.find();
+    res.status(200).json(alumnos);
   } catch (error) {
-    res.status(500).json({ message: "Error al obtener los profesores", error });
+    res.status(500).json({ message: "Error al obtener los alumnos", error });
   }
 };
 
-export const createProfesor = async (req: Request, res: Response) => {
+export const createAlumno = async (req: Request, res: Response) => {
   try {
-    const { nombre, email, teléfono } = req.body;
-    const nuevoProfesor = new profesor({ nombre, email, teléfono });
-    await nuevoProfesor.save();
-    res.status(201).json(nuevoProfesor);
+    const { nombre, apellido, teléfono, email, edad } = req.body;
+    const nuevoAlumno = new alumno({ nombre, apellido, teléfono, email, edad });
+    await nuevoAlumno.save();
+    res.status(201).json(nuevoAlumno);
   } catch (error) {
-    res.status(500).json({ message: "Error al crear el profesor", error });
+    res.status(500).json({ message: "Error al crear el alumno", error });
   }             
 }
