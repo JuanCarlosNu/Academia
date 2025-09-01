bb242d1ae880c54167c022964bebbc44a511807f
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-09-01 09:21:42 -0300

    build config for Render ram limit problem

--- Frontend/src/Components/WeekView/WeekView.js
+++ Frontend/src/Components/WeekView/WeekView.js
@@ -6,35 +6,39 @@ function WeekView({ week, onSelectDay }) {
   return (
     <div className="week-grid">
       {console.log("📅 Semana actual:", week)}
-      {week.map((dayObj) => (
-        <div
-          key={dayObj.date}
-          className="day-col"
-          onClick={() => onSelectDay(dayObj)}
-        >
-          <div className="day-header">
-            <span>
-              {dayObj.date.toLocaleDateString("es-AR", { weekday: "short" })}
-            </span>
-            <span>{dayObj.date.getDate()}</span>
-          </div>
-          {console.log("el objeto día es:", dayObj)}
+      {week.map(
+        (
+          dayObj //aquí nace el objeto dayObj
+        ) => (
+          <div
+            key={dayObj.date}
+            className="day-col"
+            onClick={() => onSelectDay(dayObj)}
+          >
+            <div className="day-header">
+              <span>
+                {dayObj.date.toLocaleDateString("es-AR", { weekday: "short" })}
+              </span>
+              <span>{dayObj.date.getDate()}</span>
+            </div>
+            {console.log("el objeto día es:", dayObj)}
 
-          {dayObj.classes.length ? (
-            dayObj.classes.map((c) => (
-              <div key={c.id} className="mini-row">
-                <div className="mini-top">
-                  <span>{c.time}</span>
-                  <span>{c.circuit}</span>
+            {dayObj.classes.length ? (
+              dayObj.classes.map((c) => (
+                <div key={c.id} className="mini-row">
+                  <div className="mini-top">
+                    <span>{c.time}</span>
+                    <span>{c.circuit}</span>
+                  </div>
+                  <div className="mini-student">{c.student}</div>
                 </div>
-                <div className="mini-student">{c.student}</div>
-              </div>
-            ))
-          ) : (
-            <p className="no-class">Sin clases</p>
-          )}
-        </div>
-      ))}
+              ))
+            ) : (
+              <p className="no-class">Sin clases</p>
+            )}
+          </div>
+        )
+      )}
     </div>
   );
 }
