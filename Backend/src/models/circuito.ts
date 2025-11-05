6cf37622a35afba002be41541bc09f281ad14eeb
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-11-05 14:19:57 -0300

    circuit data added _id in interface

--- Backend/src/models/circuito.ts
+++ Backend/src/models/circuito.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export interface ICircuito extends Document {
+  _id: Types.ObjectId;
   nombre: string;
   ubicacion: string;
  
