c799f7895c65af012302be1c327dd3cdcad54b36
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-12-05 10:11:31 -0300

    correspondant routes added

--- Backend/src/routes/alumnoRoutes.ts
+++ Backend/src/routes/alumnoRoutes.ts
@@ -1,10 +1,12 @@
-import { getAlumnos, createAlumno } from './../controllers/alumnoController';
 import express  from "express";
+import { getAlumnos, createAlumno, getAlumnoById, updateAlumno, deleteAlumno } from './../controllers/alumnoController';
  
 const router = express.Router();
 
 router.get('/', getAlumnos);
 router.post('/', createAlumno);
-
+router.get('/:id', getAlumnoById);
+router.put('/:id', updateAlumno);
+router.delete('/:id', deleteAlumno);    
 
 export default router;
