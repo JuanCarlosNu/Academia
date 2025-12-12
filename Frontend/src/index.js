2d2f3760b1b75b89232668b734a15a9cbca569eb
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-12-12 09:20:52 -0300

    AutProvider implemented

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../src/App";
 import "./index.css";
+import { AuthProvider } from "./Context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <div className="flex flex-col min-h-screen">
-      <App />
-    </div>
-  </React.StrictMode>
+  <AuthProvider>
+    <React.StrictMode>
+      <div className="flex flex-col min-h-screen">
+        <App />
+      </div>
+    </React.StrictMode>
+  </AuthProvider>
 );
