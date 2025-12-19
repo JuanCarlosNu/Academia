99719352a8ff70594d92333be094f5d4e5ed8b12
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-12-19 11:40:44 -0300

    Pago Model modified

--- Backend/src/models/pago.ts
+++ Backend/src/models/pago.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose';
 
 export interface IPago extends mongoose.Document {
-  fecha: string; // YYYY-MM-DD
+  fecha: Date; // YYYY-MM-DD
   monto: number; // Monto del pago
   metodo_pago: string; // Ej: 'efectivo', 'transferencia', 'mercadopago'
   alumno: mongoose.Types.ObjectId; // Referencia al alumno que realizó el pago
   cantidad_clases_pagadas: number; // Cantidad de clases pagadas en este pago
+  estado: "activo" | "cancelado";
 
 } 
 
 const PagoSchema = new mongoose.Schema({
   fecha: {
-    type: String, // YYYY-MM-DD
+    type: Date, // YYYY-MM-DD
     required: true,
   },
   monto: {
@@ -19,7 +20,8 @@ const PagoSchema = new mongoose.Schema({
     required: true,
   },
   metodo_pago: {
-    type: String, // ej: 'efectivo', 'transferencia', 'mercadopago'
+    type: String,
+    enum: ['efectivo', 'transferencia', 'mercadopago'],
     required: true,
   },
   alumno: {
@@ -30,6 +32,11 @@ const PagoSchema = new mongoose.Schema({
   cantidad_clases_pagadas: {
     type: Number,
     required: true,
+  },
+  estado: {
+    type: String,
+    enum: ['activo', 'cancelado'],
+    default: 'activo',
   },
 }, { timestamps: true });
 
