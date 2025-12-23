a9a1ed4aef4dfea1993aa00123b67f66b0ef382b
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2025-12-23 10:39:20 -0300

    get pagos by alumno created

--- Backend/src/controllers/pagoController.ts
+++ Backend/src/controllers/pagoController.ts
@@ -24,3 +24,12 @@ export const createPago = async (req: Request, res: Response) => {
     return res.status(500).json({ error: "Error al registrar pago" });
   }
 };
+export const getPagosByAlumno = async (req: Request, res: Response) => {
+    try {
+        const {idAlumno} = req.params;
+        const pagos = await Pago.find({ alumno: idAlumno }).sort({ fecha: -1 });
+        return res.status(200).json(pagos);} 
+        catch (error) {console.log("Error al obtener pagos:", error);
+            return res.status(500).json({ error: "Error al obtener pagos" });
+        }
+};
